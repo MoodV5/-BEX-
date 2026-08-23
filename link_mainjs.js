9c32145a82d6fd1b45398177f7cac97418c73eb7
MoodV5 <s.uto20230227@gmail.com>
2026-08-23 18:05:12 +0900

    キャッシュ対策

--- link_mainjs.js
+++ link_mainjs.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const dir = __dirname;
-const scriptTag = '<script src="main.js"></script>\n</body>';
+const scriptTag = '<script src="main.js?v=20260823-1"></script>\n</body>';
 
 fs.readdirSync(dir).forEach(file => {
     if (file.endsWith('.html')) {
         let content = fs.readFileSync(path.join(dir, file), 'utf8');
         
         // If main.js is not linked, add it before </body>
-        if (!content.includes('src="main.js"')) {
+        if (!/src=["']main\.js(?:\?[^"']*)?["']/.test(content)) {
             content = content.replace(/<\/body>/, scriptTag);
             fs.writeFileSync(path.join(dir, file), content, 'utf8');
             console.log(`Linked main.js to ${file}`);
